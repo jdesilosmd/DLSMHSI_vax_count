3a15ae08a7aacc68185756037ddebab097791e6e
jdesilosmd <jdesilosmd@gmail.com>
2021-03-18 12:59:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,42 +19,49 @@ st.sidebar.markdown('#### DOH Data Drop dataset can also be accessed [here](http
 # Upload data here:
 upload_file = st.file_uploader('Upload data here: ',
                                type=['xlsx', 'xls'])
-upload_file.seek(0)
-df = pd.read_excel(upload_file)
-df['STATUS'] = df['STATUS'].fillna(2)
-df_def = df.loc[df['STATUS'] == 0, :]
-df_vax = df.loc[df['STATUS'] == 1, :]
-df_aefi_mild = df.loc[df['AEFI'] == 1, :]
-df_aefi_sev = df.loc[df['AEFI'] == 2]
-df_unvax = df.loc[df['STATUS'] == 0, :]
-df_outvax = df.loc[df['VACCINE'] == 'OUTSIDE',:]
-df_az = df.loc[(df['VACCINE'] == 'ASTRAZENECA')
-               & (df['STATUS'] == 1), :]
-df_sino = df.loc[(df['VACCINE'] == 'SINOVAC')
-               & (df['STATUS'] == 1), :]
 
-def_count = df_def.STATUS.count()
-az_count = df_az.VACCINE.count()
-sino_count = df_sino.VACCINE.count()
-vax_count = az_count + sino_count
-aefi_mild_count = df_aefi_mild.AEFI.count()
-aefi_sev_count = df_aefi_sev.AEFI.count()
-aefi_count = aefi_mild_count + aefi_sev_count
-unvax_count = df_unvax.STATUS.count()
-out_count = df_outvax.VACCINE.count()
+if case_upload is None:
+  st.write('Please upload a dataset.')
 
+  elif case_upload is not None:
+  
+  case_upload.seek(0)
+  
+  df = pd.read_excel(upload_file)
+  df['STATUS'] = df['STATUS'].fillna(2)
+  df_def = df.loc[df['STATUS'] == 0, :]
+  df_vax = df.loc[df['STATUS'] == 1, :]
+  df_aefi_mild = df.loc[df['AEFI'] == 1, :]
+  df_aefi_sev = df.loc[df['AEFI'] == 2]
+  df_unvax = df.loc[df['STATUS'] == 0, :]
+  df_outvax = df.loc[df['VACCINE'] == 'OUTSIDE',:]
+  df_az = df.loc[(df['VACCINE'] == 'ASTRAZENECA')
+                 & (df['STATUS'] == 1), :]
+  df_sino = df.loc[(df['VACCINE'] == 'SINOVAC')
+                 & (df['STATUS'] == 1), :]
 
-
-st.markdown('# **Vaccination Figures:**')
-st.markdown('## **Vaccinated:** {}'.format(vax_count))
-st.markdown('### &ensp;&ensp;&ensp;&ensp;&ensp;**AstraZeneca:** {}'.format(az_count))
-st.markdown('### &ensp;&ensp;&ensp;&ensp;&ensp;**Sinovac:** {}'.format(sino_count))
-
-st.markdown('## **With Reported AEFI:** {}'.format(aefi_count))
-st.markdown('### &ensp;&ensp;&ensp;&ensp;&ensp; **Mild:** {}'.format(aefi_mild_count))
-st.markdown('### &ensp;&ensp;&ensp;&ensp;&ensp; **Severe:** {}'.format(aefi_sev_count))
-
-st.markdown('## **Deferred:** {}'.format(def_count))
+  def_count = df_def.STATUS.count()
+  az_count = df_az.VACCINE.count()
+  sino_count = df_sino.VACCINE.count()
+  vax_count = az_count + sino_count
+  aefi_mild_count = df_aefi_mild.AEFI.count()
+  aefi_sev_count = df_aefi_sev.AEFI.count()
+  aefi_count = aefi_mild_count + aefi_sev_count
+  unvax_count = df_unvax.STATUS.count()
+  out_count = df_outvax.VACCINE.count()
 
 
-#st.dataframe(df)
+
+  st.markdown('# **Vaccination Figures:**')
+  st.markdown('## **Vaccinated:** {}'.format(vax_count))
+  st.markdown('### &ensp;&ensp;&ensp;&ensp;&ensp;**AstraZeneca:** {}'.format(az_count))
+  st.markdown('### &ensp;&ensp;&ensp;&ensp;&ensp;**Sinovac:** {}'.format(sino_count))
+
+  st.markdown('## **With Reported AEFI:** {}'.format(aefi_count))
+  st.markdown('### &ensp;&ensp;&ensp;&ensp;&ensp; **Mild:** {}'.format(aefi_mild_count))
+  st.markdown('### &ensp;&ensp;&ensp;&ensp;&ensp; **Severe:** {}'.format(aefi_sev_count))
+
+  st.markdown('## **Deferred:** {}'.format(def_count))
+
+
+  #st.dataframe(df)
